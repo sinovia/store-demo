6f87afd00b3195b4da9b79b3cad3d5630fd92a09
SINOVIA <somayfield@gmail.com>
2016-06-29 10:23:54 -0400

    Updated

---
+++ js/app.js
@@ -0,0 +1,32 @@
+var cartDemo = angular.module('cartDemo', []);
+
+cartDemo.controller('CartForm', function($scope) {
+    $scope.invoice = {
+        items: [{
+            qty: 1,
+            description: 'Cat grass',
+            cost: 9.95}]
+    };
+
+    $scope.addItem = function() {
+        $scope.invoice.items.push({
+            qty: 1,
+            description: '',
+            cost: 0
+        });
+    },
+          $scope.removeItem = function (index) {
+            $scope.invoice.items.splice(index,1);
+    },
+
+
+
+        $scope.total = function() {
+            var total = 0;
+            angular.forEach($scope.invoice.items, function(item) {
+                total += item.qty * item.cost;
+            })
+
+            return total;
+        }
+      });
